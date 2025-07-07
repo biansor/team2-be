28ee0e56c22e528175a66dfd6478f2fceb7d0370
Biansor Almerol <biansor.almerol@gmail.com>
2025-07-07 22:58:47 +0800

    update model format

--- app.py
+++ app.py
@@ -238,17 +238,22 @@ def compare_faces_base64():
 @app.route('/models', methods=['GET'])
 def get_available_models():
     """Get list of available face recognition models"""
-    models = [
-        'VGG-Face',
-        'Facenet',
-        'Facenet512',
-        'OpenFace',
-        'DeepFace',
-        'DeepID',
-        'ArcFace',
-        'Dlib',
-        'SFace'
-    ]
+    models = {
+        "VGG-Face": {
+            "cosine": 0.68,
+            "euclidean": 1.17,
+            "euclidean_l2": 1.17,
+        },
+        "Facenet": {"cosine": 0.40, "euclidean": 10, "euclidean_l2": 0.80},
+        "Facenet512": {"cosine": 0.30, "euclidean": 23.56, "euclidean_l2": 1.04},
+        "ArcFace": {"cosine": 0.68, "euclidean": 4.15, "euclidean_l2": 1.13},
+        "Dlib": {"cosine": 0.07, "euclidean": 0.6, "euclidean_l2": 0.4},
+        "SFace": {"cosine": 0.593, "euclidean": 10.734, "euclidean_l2": 1.055},
+        "OpenFace": {"cosine": 0.10, "euclidean": 0.55, "euclidean_l2": 0.55},
+        "DeepFace": {"cosine": 0.23, "euclidean": 64, "euclidean_l2": 0.64},
+        "DeepID": {"cosine": 0.015, "euclidean": 45, "euclidean_l2": 0.17},
+        "GhostFaceNet": {"cosine": 0.65, "euclidean": 35.71, "euclidean_l2": 1.10}
+    }
     
     distance_metrics = ['cosine', 'euclidean', 'euclidean_l2']
     
